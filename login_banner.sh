ea0de69d99f280ab01bd3b31908575ba9100fa11
Eleanor Coyle <coyleej@protonmail.com>
2019-05-08 16:55:38 -0400

    Bug fix. login_banner.sh now checks for the presence of a display manager before executing some commands.

--- login_banner.sh
+++ login_banner.sh
@@ -9,7 +9,7 @@
 #       OPTIONS:  ---
 #  REQUIREMENTS:  ---
 #          BUGS:  ---
-#         NOTES:  ---
+#         NOTES:  The control nodes do not have a display manager installed.
 #        AUTHOR:  Eleanor Coyle, coyleej@protonmail.com
 #       COMPANY:  Azimuth Corporation
 #       VERSION:  1.0
@@ -24,43 +24,54 @@ sudo cat banner_text.txt >> /etc/issue
 sudo cat banner_text_short.txt >> /etc/issue.net
 sudo chown root: /etc/issue /etc/issue.net
 
-# Disable wayland or it kills the nvidia drivers
-customConf=/etc/gdm3/custom.conf
-sudo sed -i.bak -e "/WaylandEnable/ s/^#//" $customConf
+if [ -d /etc/dconf ] && (dpkg -l | grep gdm); then
+	# Applies to everything but the control nodes
+	echo "Display manager installed. Setting up banner."
 
-if ( ! grep "^WaylandEnable=false" $customConf ); then
-	echo "Error: "$customConf" did not update successfully!"
-	echo "Error: Wayland is still enabled, will cause login issues!"
-	echo ""
+	# Disable wayland or it kills the nvidia drivers
+	customConf=/etc/gdm3/custom.conf
+	sudo sed -i.bak -e "/WaylandEnable/ s/^#//" $customConf
+
+	if ( ! grep "^WaylandEnable=false" $customConf ); then
+		echo "Error: "$customConf" did not update successfully!"
+		echo "Error: Wayland is still enabled, will cause login issues!"
+		echo ""
+	fi
+
+	# Create directory and files
+	gdmProfile=/etc/dconf/profile/gdm
+	gdmDir=/etc/dconf/db/gdm.d
+	gdmBanner=$gdmDir/01-banner-message
+
+	sudo touch $gdmProfile
+	sudo mkdir -p $gdmDir
+	sudo touch $gdmBanner
+
+	sudo chown $USER: $gdmProfile $gdmBanner
+
+	# Create dconf profile
+	sudo echo "user-db:user" > $gdmProfile
+	sudo echo "system-db:gdm" >> $gdmProfile
+	sudo echo "file-db:/usr/share/gdm/greeter-dconf-defaults" >> $gdmProfile
+
+	# Create banner message
+	sudo echo "[org/gnome/login-screen]" > $gdmBanner
+	sudo echo "banner-message-enable=true" >> $gdmBanner
+	sudo echo "banner-message-text='I have read & consent to terms in IS user agreement.'" >> $gdmBanner
+
+	sudo chown root: $gdmProfile $gdmBanner
+
+	# Reconfigure things
+	echo "Updated files, now reconfiguring..."
+	sudo dconf update
+	sudo dpkg-reconfigure gdm3
+
+	# Notify user that a reboot is required.
+	echo "This workstation must be restarted for these changes to take effect"
+
+else
+	# Control nodes only
+	echo "No display manager installed"
+	echo "Only /etc/issue and /etc/issue.net updated"
+
 fi
-
-# Create directory and files
-gdmProfile=/etc/dconf/profile/gdm
-gdmDir=/etc/dconf/db/gdm.d
-gdmBanner=$gdmDir/01-banner-message
-
-sudo touch $gdmProfile
-sudo mkdir -p $gdmDir
-sudo touch $gdmBanner
-
-sudo chown $USER: $gdmProfile $gdmBanner
-
-# Create dconf profile
-sudo echo "user-db:user" > $gdmProfile
-sudo echo "system-db:gdm" >> $gdmProfile
-sudo echo "file-db:/usr/share/gdm/greeter-dconf-defaults" >> $gdmProfile
-
-# Create banner message
-sudo echo "[org/gnome/login-screen]" > $gdmBanner
-sudo echo "banner-message-enable=true" >> $gdmBanner
-sudo echo "banner-message-text='I have read & consent to terms in IS user agreement.'" >> $gdmBanner
-
-sudo chown root: $gdmProfile $gdmBanner
-
-# Reconfigure things
-sudo dconf update
-sudo dpkg-reconfigure gdm3
-
-# Notify user that a reboot is required.
-echo "This workstation must be restarted for these changes to take effect"
-
