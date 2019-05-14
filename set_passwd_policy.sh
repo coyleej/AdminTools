7866d190452eeab70e3536e484c86fbf3e6b18eb
Eleanor Coyle <coyleej@protonmail.com>
2019-05-14 10:24:30 -0400

    Removed all set_passwd_policy debugging statements.

--- set_passwd_policy.sh
+++ set_passwd_policy.sh
@@ -20,21 +20,19 @@
 #===================================================================================
 
 # SET PASSWORD POLICY
-#logindefs="/etc/login.defs"
-#pwquality="/etc/security/pwquality.conf"
-logindefs="login.defs"
-pwquality="pwquality.conf"
+logindefs="/etc/login.defs"
+pwquality="/etc/security/pwquality.conf"
 hbssSetup="install.sh"
 
 # Setup HBSS
 echo "Installing McAfee and HBSS ..."
-#sudo bash $hbssSetup -i
+sudo bash $hbssSetup -i
 
 echo ""
 echo "Adjusting password requirements ..."
 
-sudo chown $USER: pwquality.conf login.defs
-sudo chmod 664 pwquality.conf login.defs
+sudo chown $USER: $pwquality $logindefs
+sudo chmod 664 $pwquality $logindefs
 
 sudo sed -i.bak -e "/^PASS_MAX_DAYS/ s/[0-9].*/60/" \
 	-e "/^PASS_WARN_AGE/ s/[0-9].*/7/" \
@@ -51,10 +49,8 @@ sudo sed -i.bak -e "/^# difok/ c\difok = 2" \
 	-e "/^# usercheck/ c\usercheck = 1 " \
 	$pwquality
 
-#sudo sed -i.bak -e "/^# difok/ c\difok = 2/" -e "/^# minlen/ c\minlen = 15/" -e "/^# dcredit/ c\dcredit = -2/" -e "/^# ucredit/ c\ucredit = -2/" -e "/^# lcredit/ c\lcredit = -2/" -e "/^# ocredit/ c\ocredit = -2/" -e "/^# minclass/ c\minclass = 4/" -e "/^# maxrepeat/ c\maxrepeat = 2/" -e "/^# usercheck/ c\usercheck = 1/ " $pwquality
-
-sudo chown root: pwquality.conf login.defs
-sudo chmod 644 pwquality.conf login.defs
+sudo chown root: $pwquality $logindefs
+sudo chmod 644 $pwquality $logindefs
 
 echo
 echo "Please set your new password. It must meet the following criteria:"
@@ -67,7 +63,7 @@ echo "* Not contain more than two of the same character in a row"
 echo "* Contain at least 2 characters not in the previous password"
 echo "* Not contain your username"
 
-#passwd
+passwd
 
 if [ $? = 0 ]; then
 	echo "Successfully set new password"
