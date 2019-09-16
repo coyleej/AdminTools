c7526128b614f8e68cec85e521b2f39bbf99c808
Eleanor Coyle <coyleej@protonmail.com>
2019-09-16 15:49:59 -0400

    set_kernel_version.sh prevented from writing -1 as a grub entry

--- set_kernel_version.sh
+++ set_kernel_version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 newKernel="4.15.0-62-generic"
+grubFile="/etc/default/grub"
+grubCfg="/boot/grub/grub.cfg"
+kernelFam=$(echo $newKernel | sed 's/\.0.*$//')
 
-sudo apt update && sudo apt upgrade
+sudo apt update; sudo apt upgrade
 
 # Check if desired kernel is currently in use
 # Script exits if true
@@ -19,25 +22,52 @@ if [ $? != 0 ]; then
 	sudo apt install linux-image-$newKernel linux-headers-$newKernel
 fi
 
-# Determine which grub entry it is and adjust to grub's 0-based indexing
-grubEntry=$(sed -e 's/--.*//' -e '/menuentry.*Linux/ !d' /boot/grub/grub.cfg \
+# Determine which grub entry it is
+#grep "menuentry" /boot/grub/grub.cfg | sed -e "1,4 d" -e "s/--.*//"
+
+grubEntry=$(sed -e 's/--.*//' -e '/menuentry.*Linux/ !d' $grubCfg \
 	| grep -n "$newKernel" | sed -e '/recovery/ d' -e 's/:.*//')
+
+# Check that $grubEntry is not NULL. If it is, try a different approach:
+if [ ! $grubEntry ]; then
+
+       if (grep $kernelFam $grubCfg > /dev/null); then
+		# Grab the latest kernel in that family (e.g. 4.15) available
+
+		echo "Grabbing latest similar kernel"
+		grubEntry=$(sed -e 's/--.*//' -e '/menuentry.*Linux/ !d' $grubCfg \
+			| grep -n "$kernelFam" | sed -e '/recovery/ d' -e 's/:.*//')
+		grubEntry=$(echo $grubEntry | cut -d ' ' -f 1)
+
+	else
+		# Grab the oldest kernel in the file
+		# The -1 is to ignore the recovery mode entry
+		# NOTE: this is a catch-all that should never have to run
+
+		echo "Grabbing oldest kernel (catch-all should other conditions fail)"
+		grubEntry=$(( $(grep -c "^\smenuentry" $grubCfg) - 1 ))
+	fi
+fi
+
+# Adjust to grub's 0-based indexing
 grubEntry=$(( $grubEntry - 1 ))
 
-echo "Updating grub file"
+echo ""
+echo "Using grub entry 1>$grubEntry"
+echo ""
 
 # Adjust grub file
-#grubFile="/home/$USER/test.txt"     ## TEST
-grubFile="/etc/default/grub"
+if [ $grubEntry -ge 0 ]; then
+	sudo cp -v $grubFile $grubFile.bak
+	sudo sed -i "/^GRUB_DEFAULT=/ s/=.*/=\"1\>$grubEntry\"/" $grubFile
+	sudo update-grub
+	echo "Reboot required!!"
+	echo "Confirm that changes to $grubFile are correct before rebooting."
+	echo "After reboot, check that you are running $newKernel with 'uname -r"
+##	sudo shutdown -r +2 "Computer will reboot in 1 minute!"
 
-sudo cp -v $grubFile $grubFile.bak
-sudo sed -i "/^GRUB_DEFAULT=/ s/=.*/=\"1\>$grubEntry\"/" $grubFile
-
-sudo update-grub
-
-echo ""
-echo "Reboot required!!"
-echo "Confirm that changes are correct before rebooting."
-
-#sudo shutdown -r +1 "Computer will reboot in 1 minute!"
-
+else
+	echo "ERROR: Negative grub entry detected."
+	echo "Please check the file manually or ask your admin for help!"
+	exit 1
+fi
